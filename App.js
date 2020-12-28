bca783c4f4d9d08399c6dc8a5209eda91947c584
Olorunda_Samuel <olorundasamuel2@gmail.com>
2020-12-28 02:52:05 +0100

    A Project on Learning the CRUD method uding React And Firebase

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from "react"
+import './App.css';
+import CrudOpp from './Components/CrudOpp/CrudOpp';
+// import TextFieldess from "./Components/MaterialUI/TextFieldess"
+
+function App() {
+  return (
+    <div >
+      <CrudOpp />
+      {/* <TextFieldess /> */}
+    </div>
+  );
+}
+
+export default App;
+
